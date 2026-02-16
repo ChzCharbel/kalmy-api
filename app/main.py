c5a442436ceccafbed7f2bfd69e9f0f8ac7a451f
ChzCharbel <charbel_chz@outlook.com>
2026-02-16 16:19:41 -0600

    feat: Se agrego el modelo Item de la bd

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from .database import engine, Base
+from . import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Kalmy API",
     description="A simple FastAPI application for Kalmy",
-    version="0.1.0"
+    version="0.2.0"
 )
 
 @app.get("/")
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Boolean, Float, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from .database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True
+    )
+    name: Mapped[str] = mapped_column(
+        String, nullable=False
+    )
+    description: Mapped[str] = mapped_column(
+        String, nullable=False
+    )
+    price: Mapped[float] = mapped_column(
+        Float, nullable=False
+    )
+    available: Mapped[bool] = mapped_column(
+        Boolean, default=True
+    )
